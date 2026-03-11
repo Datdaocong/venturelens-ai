c10078257482f03273cda613aaa3c91e52be9c58
Dao Cong Dat <datdc.b25tv081@stu.ptit.edu.vn>
2026-03-11 23:24:11 +0700

    update for deployment

--- utils/gemini_client.py
+++ utils/gemini_client.py
@@ -1,15 +1,20 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import streamlit as st
 
 load_dotenv()
 
 
 def get_gemini_client():
-    api_key = os.getenv("GEMINI_API_KEY")
+    api_key = None
+
+    if "GEMINI_API_KEY" in st.secrets:
+        api_key = st.secrets["GEMINI_API_KEY"]
+    else:
+        api_key = os.getenv("GEMINI_API_KEY")
 
     if not api_key:
-        raise ValueError("GEMINI_API_KEY not found in .env")
+        raise ValueError("GEMINI_API_KEY not found in Streamlit secrets or .env")
 
-    client = genai.Client(api_key=api_key)
-    return client
+    return genai.Client(api_key=api_key)
